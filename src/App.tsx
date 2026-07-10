1c78bec446a148d810eabd09ea9e4bd4698fb21f
amores <amores.dev@outlook.com>
2026-07-10 01:18:23 -0600

    feat: Initialize React project with Vite, TypeScript, and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold">API Tasks</h1>
+      <Button>Funciona</Button>
+    </div>
+  );
+}
+
+export default App;
